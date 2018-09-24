3e07cd0212ee3e594a376abf2166eb5fe4130ec3
s-chenjeff <s-chenjeff@53S50023207.it.bsd405.org>
2018-09-24 15:30:29 -0700

    I turned in Project I 4 days late! Why?

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,17 +1,34 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
+public class ASCIIArt {
+	public static void prinsenVlag()
+	{
+		oranje();
+		wit();
+		blauw();
+	}
+	public static void line()
+	{
+		System.out.println("+--------------------+");
+	}
+	public static void oranje()
+	{
+		line();
+		System.out.println("|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\|");
+		line();
+	}
+	public static void wit()
+	{
+		System.out.println("|                    |");
+	}
+	public static void blauw()
+	{
+		line();
+		System.out.println("|\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"|");
+		line();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		prinsenVlag();
+		System.out.println("God, Nederland, Oranje");
+	}
 
-// YOUR CODE GOES HERE :)
+}
